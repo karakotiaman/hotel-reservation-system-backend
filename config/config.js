e313e78a8374ccdae9bbaf237456c89b3b7187b9
karakotiaman <karakotiaman@gmail.com>
2025-03-20 23:24:30 +0530

    config changes

--- config/config.js
+++ config/config.js
@@ -2,12 +2,17 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "port": process.env.DB_PORT,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT,
+    'username': process.env.DB_USER,
+    'password': process.env.DB_PASSWORD,
+    'database': process.env.DB_NAME,
+    'host': process.env.DB_HOST,
+    'port':process.env.DB_PORT,
+    'dialect': 'mysql',
+    'max': 20, // maximum connection which postgresql or mysql can initiate
+    'min': 0, // minimum connection which postgresql or mysql can initiate
+    'acquire': 20000, // time require to reconnect
+    'idle': 20000, // get idle connection
+    'evict': 10000
   },
   "test": {
     "username": process.env.DB_USER,
@@ -18,11 +23,16 @@ module.exports = {
     "dialect": process.env.DB_DIALECT,
   },
   "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "port": process.env.DB_PORT,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT,
+    'username': process.env.DB_USER,
+    'password': process.env.DB_PASSWORD,
+    'database': process.env.DB_NAME,
+    'host': process.env.DB_HOST,
+    'port':process.env.DB_PORT,
+    'dialect': 'mysql',
+    'max': 20, // maximum connection which postgresql or mysql can initiate
+    'min': 0, // minimum connection which postgresql or mysql can initiate
+    'acquire': 20000, // time require to reconnect
+    'idle': 20000, // get idle connection
+    'evict': 10000
   }
 }
